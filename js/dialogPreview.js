7d7780e3418d9afedb5c5915627cbdbf379100bb
SKYFish17 <nwcorp93@yandex.ru>
2020-10-24 13:53:48 +0300

    const/let, object-shortland es6, arrow functions

--- js/dialogPreview.js
+++ js/dialogPreview.js
@@ -1,47 +1,47 @@
 'use strict';
 
-(function () {
-  var bigPicture = document.querySelector('.big-picture');
-  var bigPictureCloseBtn = bigPicture.querySelector('.big-picture__cancel');
-  var commentsList = bigPicture.querySelector('.social__comments');
+(() => {
+  const bigPicture = document.querySelector(`.big-picture`);
+  const bigPictureCloseBtn = bigPicture.querySelector(`.big-picture__cancel`);
+  const commentsList = bigPicture.querySelector(`.social__comments`);
 
-  var openBigPicture = function (imgSrc, usersPosts) {
+  const openBigPicture = (imgSrc, usersPosts) => {
     window.dialog.openModal();
-    commentsList.innerHTML = '';
-    bigPicture.classList.remove('hidden');
+    commentsList.innerHTML = ``;
+    bigPicture.classList.remove(`hidden`);
 
-    for (var i = 0; i < window.constants.NUMBER_OF_POSTS; i++) {
+    for (let i = 0; i < window.constants.NUMBER_OF_POSTS; i++) {
       if (imgSrc === usersPosts[i].url) {
         window.preview.render(usersPosts[i]);
       }
     }
-    document.addEventListener('keydown', onBigPictureEscPress);
+    document.addEventListener(`keydown`, onBigPictureEscPress);
   };
 
-  var closeBigPicture = function () {
-    bigPicture.classList.add('hidden');
+  const closeBigPicture = () => {
+    bigPicture.classList.add(`hidden`);
     window.dialog.closeModal();
-    document.removeEventListener('keydown', onBigPictureEscPress);
+    document.removeEventListener(`keydown`, onBigPictureEscPress);
   };
 
-  bigPictureCloseBtn.addEventListener('click', function () {
+  bigPictureCloseBtn.addEventListener(`click`, () => {
     closeBigPicture();
   });
 
-  bigPictureCloseBtn.addEventListener('keydown', function (evt) {
-    if (evt.code === 'Enter') {
+  bigPictureCloseBtn.addEventListener(`keydown`, (evt) => {
+    if (evt.code === `Enter`) {
       closeBigPicture();
     }
   });
 
-  var onBigPictureEscPress = function (evt) {
-    if (evt.code === 'Escape') {
+  const onBigPictureEscPress = (evt) => {
+    if (evt.code === `Escape`) {
       evt.preventDefault();
       closeBigPicture();
     }
   };
 
   window.dialogPreview = {
-    openBigPicture: openBigPicture
+    openBigPicture
   };
 })();
