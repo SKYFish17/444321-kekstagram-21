ef6118d98665dda9e75b273138ba5c93b59368af
SKYFish17 <nwcorp93@yandex.ru>
2020-10-24 13:58:39 +0300

    const/let, object-shortland es6, arrow functions

--- js/formScale.js
+++ js/formScale.js
@@ -1,64 +1,64 @@
 'use strict';
 
-(function () {
-  var imgUploadContainer = document.querySelector('.img-upload');
-  var imgUploadOverlay = imgUploadContainer.querySelector('.img-upload__overlay');
-  var imgUploadPreviewContainer = imgUploadOverlay.querySelector('.img-upload__preview');
-  var scaleContainer = imgUploadContainer.querySelector('.scale');
-  var scaleInput = scaleContainer.querySelector('.scale__control--value');
+(() => {
+  const imgUploadContainer = document.querySelector(`.img-upload`);
+  const imgUploadOverlay = imgUploadContainer.querySelector(`.img-upload__overlay`);
+  const imgUploadPreviewContainer = imgUploadOverlay.querySelector(`.img-upload__preview`);
+  const scaleContainer = imgUploadContainer.querySelector(`.scale`);
+  const scaleInput = scaleContainer.querySelector(`.scale__control--value`);
 
-  var changeScaleValue = function (sign) {
-    var scaleValue = scaleInput.value.split('%');
+  const changeScaleValue = (sign) => {
+    const scaleValue = scaleInput.value.split(`%`);
 
     switch (sign) {
-      case '+':
-        scaleInput.value = parseInt(scaleValue[0], 10) + window.constants.SCALE_STEP + '%';
-        imgUploadPreviewContainer.style.transform = 'scale' + '(' + (parseInt(scaleValue[0], 10) + window.constants.SCALE_STEP) / 100 + ')';
+      case `+`:
+        scaleInput.value = parseInt(scaleValue[0], 10) + window.constants.SCALE_STEP + `%`;
+        imgUploadPreviewContainer.style.transform = `scale` + `(` + (parseInt(scaleValue[0], 10) + window.constants.SCALE_STEP) / 100 + `)`;
         break;
-      case '-':
-        scaleInput.value = parseInt(scaleValue[0], 10) - window.constants.SCALE_STEP + '%';
-        imgUploadPreviewContainer.style.transform = 'scale' + '(' + (parseInt(scaleValue[0], 10) - window.constants.SCALE_STEP) / 100 + ')';
+      case `-`:
+        scaleInput.value = parseInt(scaleValue[0], 10) - window.constants.SCALE_STEP + `%`;
+        imgUploadPreviewContainer.style.transform = `scale` + `(` + (parseInt(scaleValue[0], 10) - window.constants.SCALE_STEP) / 100 + `)`;
         break;
     }
   };
 
-  var resetScaleValue = function () {
-    imgUploadPreviewContainer.style.transform = 'scale' + '(' + 1 + ')';
+  const resetScaleValue = () => {
+    imgUploadPreviewContainer.style.transform = `scale` + `(` + 1 + `)`;
   };
 
-  var onScaleBiggerClick = function () {
-    if (scaleInput.value !== (window.constants.MAX_SCALE + '%')) {
-      changeScaleValue('+');
+  const onScaleBiggerClick = () => {
+    if (scaleInput.value !== (window.constants.MAX_SCALE + `%`)) {
+      changeScaleValue(`+`);
     }
   };
 
-  var onScaleBiggerPressEnter = function (evt) {
-    if (evt.code === 'Enter') {
-      if (scaleInput.value !== (window.constants.MAX_SCALE + '%')) {
-        changeScaleValue('+');
+  const onScaleBiggerPressEnter = (evt) => {
+    if (evt.code === `Enter`) {
+      if (scaleInput.value !== (window.constants.MAX_SCALE + `%`)) {
+        changeScaleValue(`+`);
       }
     }
   };
 
-  var onScaleSmallerClick = function () {
-    if (scaleInput.value !== (window.constants.MIN_SCALE + '%')) {
-      changeScaleValue('-');
+  const onScaleSmallerClick = () => {
+    if (scaleInput.value !== (window.constants.MIN_SCALE + `%`)) {
+      changeScaleValue(`-`);
     }
   };
 
-  var onScaleSmallerPressEnter = function (evt) {
-    if (evt.code === 'Enter') {
-      if (scaleInput.value !== (window.constants.MIN_SCALE + '%')) {
-        changeScaleValue('-');
+  const onScaleSmallerPressEnter = (evt) => {
+    if (evt.code === `Enter`) {
+      if (scaleInput.value !== (window.constants.MIN_SCALE + `%`)) {
+        changeScaleValue(`-`);
       }
     }
   };
 
   window.formScale = {
-    onScaleBiggerClick: onScaleBiggerClick,
-    onScaleBiggerPressEnter: onScaleBiggerPressEnter,
-    onScaleSmallerClick: onScaleSmallerClick,
-    onScaleSmallerPressEnter: onScaleSmallerPressEnter,
-    resetScaleValue: resetScaleValue
+    onScaleBiggerClick,
+    onScaleBiggerPressEnter,
+    onScaleSmallerClick,
+    onScaleSmallerPressEnter,
+    resetScaleValue
   };
 })();
