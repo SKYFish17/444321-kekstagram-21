de938d5394fc2ce588ece118fd9f840dc4df2e32
SKYFish17 <nwcorp93@yandex.ru>
2020-10-24 14:11:46 +0300

    const/let, object-shortland es6, arrow functions

--- js/formEffects.js
+++ js/formEffects.js
@@ -1,30 +1,30 @@
 'use strict';
 
-(function () {
-  var imgUploadContainer = document.querySelector('.img-upload');
-  var imgUploadOverlay = imgUploadContainer.querySelector('.img-upload__overlay');
-  var imgUploadPreviewContainer = imgUploadOverlay.querySelector('.img-upload__preview');
-  var imgUploadPreview = imgUploadPreviewContainer.querySelector('img');
-  var effectSlider = imgUploadOverlay.querySelector('.effect-level');
-  var effectLevelContainer = imgUploadOverlay.querySelector('.effect-level');
-  var effectLevelPin = effectLevelContainer.querySelector('.effect-level__pin');
-  var effectLevelDepth = effectLevelContainer.querySelector('.effect-level__depth');
-  var effectLevelValue = effectLevelContainer.querySelector('.effect-level__value');
+(() => {
+  const imgUploadContainer = document.querySelector(`.img-upload`);
+  const imgUploadOverlay = imgUploadContainer.querySelector(`.img-upload__overlay`);
+  const imgUploadPreviewContainer = imgUploadOverlay.querySelector(`.img-upload__preview`);
+  const imgUploadPreview = imgUploadPreviewContainer.querySelector(`img`);
+  const effectSlider = imgUploadOverlay.querySelector(`.effect-level`);
+  const effectLevelContainer = imgUploadOverlay.querySelector(`.effect-level`);
+  const effectLevelPin = effectLevelContainer.querySelector(`.effect-level__pin`);
+  const effectLevelDepth = effectLevelContainer.querySelector(`.effect-level__depth`);
+  const effectLevelValue = effectLevelContainer.querySelector(`.effect-level__value`);
 
-  var previousEffectName = '';
+  let previousEffectName = ``;
 
-  var onEffectsItemClick = function (evt) {
-    var effectLabel = evt.target.nextElementSibling.querySelector('.effects__preview');
-    var effectName = effectLabel.classList[1];
+  const onEffectsItemClick = (evt) => {
+    const effectLabel = evt.target.nextElementSibling.querySelector(`.effects__preview`);
+    const effectName = effectLabel.classList[1];
 
     if (previousEffectName) {
       imgUploadPreview.classList.remove(previousEffectName);
     }
 
-    if (effectName === 'effects__preview--none') {
-      effectSlider.classList.add('hidden');
+    if (effectName === `effects__preview--none`) {
+      effectSlider.classList.add(`hidden`);
     } else {
-      effectSlider.classList.remove('hidden');
+      effectSlider.classList.remove(`hidden`);
     }
 
     previousEffectName = effectName;
@@ -32,29 +32,29 @@
     changeEffectLevel(window.constants.END_PIN_POSITION);
   };
 
-  var resetEffects = function () {
-    imgUploadPreview.classList = '';
-    imgUploadPreview.style.filter = '';
+  const resetEffects = () => {
+    imgUploadPreview.classList = ``;
+    imgUploadPreview.style.filter = ``;
   };
 
-  var getFilter = function (effectType, effectMinLevel, effectMaxLevel, unit, pinPosition) {
-    var effectLevel;
-    var filter;
-    var effectLevelDifference = effectMaxLevel - effectMinLevel;
+  const getFilter = (effectType, effectMinLevel, effectMaxLevel, unit, pinPosition) => {
+    let effectLevel;
+    let filter;
+    const effectLevelDifference = effectMaxLevel - effectMinLevel;
 
     effectLevel = effectMinLevel + pinPosition / window.constants.END_PIN_POSITION * effectLevelDifference;
 
-    if (unit !== 'none') {
-      filter = effectType + '(' + effectLevel + unit + ')';
+    if (unit !== `none`) {
+      filter = effectType + `(` + effectLevel + unit + `)`;
     } else {
-      filter = effectType + '(' + effectLevel + ')';
+      filter = effectType + `(` + effectLevel + `)`;
     }
 
     return filter;
   };
 
-  var getRatio = function (numberOne, numberTwo, sign) {
-    var ratio;
+  const getRatio = (numberOne, numberTwo, sign) => {
+    let ratio;
 
     if (sign) {
       ratio = numberOne / numberTwo * window.constants.MAX_PERCENT + sign;
@@ -65,46 +65,46 @@
     return ratio;
   };
 
-  var renderActualEffectLevel = function (pinPosition) {
-    effectLevelPin.style.left = getRatio(pinPosition, window.constants.END_PIN_POSITION, '%');
-    effectLevelDepth.style.width = getRatio(pinPosition, window.constants.END_PIN_POSITION, '%');
+  const renderActualEffectLevel = (pinPosition) => {
+    effectLevelPin.style.left = getRatio(pinPosition, window.constants.END_PIN_POSITION, `%`);
+    effectLevelDepth.style.width = getRatio(pinPosition, window.constants.END_PIN_POSITION, `%`);
     effectLevelValue.value = getRatio(pinPosition, window.constants.END_PIN_POSITION);
   };
 
-  var changeEffectLevel = function (pinPosition) {
+  const changeEffectLevel = (pinPosition) => {
 
     switch (imgUploadPreview.className) {
-      case 'effects__preview--none':
-        imgUploadPreview.style.filter = '';
+      case `effects__preview--none`:
+        imgUploadPreview.style.filter = ``;
         break;
-      case 'effects__preview--chrome':
-        imgUploadPreview.style.filter = getFilter('grayscale', 0, 1, 'none', pinPosition);
+      case `effects__preview--chrome`:
+        imgUploadPreview.style.filter = getFilter(`grayscale`, 0, 1, `none`, pinPosition);
         break;
-      case 'effects__preview--sepia':
-        imgUploadPreview.style.filter = getFilter('sepia', 0, 1, 'none', pinPosition);
+      case `effects__preview--sepia`:
+        imgUploadPreview.style.filter = getFilter(`sepia`, 0, 1, `none`, pinPosition);
         break;
-      case 'effects__preview--marvin':
-        imgUploadPreview.style.filter = getFilter('invert', 0, 100, '%', pinPosition);
+      case `effects__preview--marvin`:
+        imgUploadPreview.style.filter = getFilter(`invert`, 0, 100, `%`, pinPosition);
         break;
-      case 'effects__preview--phobos':
-        imgUploadPreview.style.filter = getFilter('blur', 0, 3, 'px', pinPosition);
+      case `effects__preview--phobos`:
+        imgUploadPreview.style.filter = getFilter(`blur`, 0, 3, `px`, pinPosition);
         break;
-      case 'effects__preview--heat':
-        imgUploadPreview.style.filter = getFilter('brightness', 1, 3, 'none', pinPosition);
+      case `effects__preview--heat`:
+        imgUploadPreview.style.filter = getFilter(`brightness`, 1, 3, `none`, pinPosition);
         break;
     }
     renderActualEffectLevel(pinPosition);
   };
 
-  effectLevelPin.addEventListener('mousedown', function (evt) {
+  effectLevelPin.addEventListener(`mousedown`, (evt) => {
 
-    var startCoords = {
+    let startCoords = {
       x: evt.clientX,
     };
 
-    var onEffectLevelPinMouseMove = function (moveEvt) {
+    const onEffectLevelPinMouseMove = (moveEvt) => {
 
-      var shift = {
+      const shift = {
         x: startCoords.x - moveEvt.clientX,
       };
 
@@ -112,25 +112,25 @@
         x: moveEvt.clientX,
       };
 
-      var pinPosition = effectLevelPin.offsetLeft - shift.x;
+      const pinPosition = effectLevelPin.offsetLeft - shift.x;
 
       if (pinPosition >= window.constants.START_PIN_POSITION && pinPosition <= window.constants.END_PIN_POSITION) {
         changeEffectLevel(pinPosition);
       }
     };
 
-    var onEffectLevelPinMouseUp = function () {
+    const onEffectLevelPinMouseUp = () => {
 
-      document.removeEventListener('mousemove', onEffectLevelPinMouseMove);
-      document.removeEventListener('mouseup', onEffectLevelPinMouseUp);
+      document.removeEventListener(`mousemove`, onEffectLevelPinMouseMove);
+      document.removeEventListener(`mouseup`, onEffectLevelPinMouseUp);
     };
 
-    document.addEventListener('mousemove', onEffectLevelPinMouseMove);
-    document.addEventListener('mouseup', onEffectLevelPinMouseUp);
+    document.addEventListener(`mousemove`, onEffectLevelPinMouseMove);
+    document.addEventListener(`mouseup`, onEffectLevelPinMouseUp);
   });
 
   window.formEffects = {
-    onEffectsItemClick: onEffectsItemClick,
-    resetEffects: resetEffects
+    onEffectsItemClick,
+    resetEffects
   };
 })();
