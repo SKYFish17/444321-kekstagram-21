7c391b43796c55c001b249122589a4a834e355c5
SKYFish17 <nwcorp93@yandex.ru>
2020-10-24 13:49:11 +0300

    const/let, object-shortland es6, arrow functions

--- js/dialog.js
+++ js/dialog.js
@@ -1,18 +1,18 @@
 'use strict';
 
-(function () {
-  var body = document.querySelector('body');
+(() => {
+  const body = document.querySelector(`body`);
 
-  var openModal = function () {
-    body.classList.add('modal-open');
+  const openModal = () => {
+    body.classList.add(`modal-open`);
   };
 
-  var closeModal = function () {
-    body.classList.remove('modal-open');
+  const closeModal = () => {
+    body.classList.remove(`modal-open`);
   };
 
   window.dialog = {
-    openModal: openModal,
-    closeModal: closeModal
+    openModal,
+    closeModal
   };
 })();
