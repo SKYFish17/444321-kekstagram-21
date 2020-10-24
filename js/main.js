37524905c77dfc2c14c0f9bc5a1ccee1dbd76f85
SKYFish17 <nwcorp93@yandex.ru>
2020-10-24 14:19:51 +0300

    const/let, object-shortland es6, arrow functions

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 'use strict';
 
-(function () {
-  var imgUploadContainer = document.querySelector('.img-upload');
-  var imgUploadInput = imgUploadContainer.querySelector('.img-upload__input');
-  var picturesContainer = document.querySelector('.pictures');
+(() => {
+  const imgUploadContainer = document.querySelector(`.img-upload`);
+  const imgUploadInput = imgUploadContainer.querySelector(`.img-upload__input`);
+  const picturesContainer = document.querySelector(`.pictures`);
 
-  imgUploadInput.addEventListener('change', function () {
+  imgUploadInput.addEventListener(`change`, () => {
     window.dialogForm.openUploadOverlay();
   });
 
-  var setPicturesHandlers = function (data) {
-    picturesContainer.addEventListener('click', function (evt) {
-      if (evt.target.classList.contains('picture__img')) {
+  const setPicturesHandlers = (data) => {
+    picturesContainer.addEventListener(`click`, (evt) => {
+      if (evt.target.classList.contains(`picture__img`)) {
         window.dialogPreview.openBigPicture(evt.target.attributes.src.value, data);
       }
     }, true);
 
 
-    picturesContainer.addEventListener('keydown', function (evt) {
-      var pictureAtLink = evt.target.querySelector('.picture__img');
+    picturesContainer.addEventListener(`keydown`, (evt) => {
+      const pictureAtLink = evt.target.querySelector(`.picture__img`);
 
-      if (evt.code === 'Enter') {
+      if (evt.code === `Enter`) {
         window.dialogPreview.openBigPicture(pictureAtLink.attributes.src.value, data);
       }
     }, true);
   };
 
   window.main = {
-    setPicturesHandlers: setPicturesHandlers
+    setPicturesHandlers
   };
 })();
