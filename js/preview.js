ad8283597e8942af01112742a54acf7d3297e730
SKYFish17 <nwcorp93@yandex.ru>
2020-11-11 08:28:07 +0300

    модифицирует html для удобной индикации кол-ва комментариев, ф-я для отображения непоказанных комментариев + обработчик

--- js/preview.js
+++ js/preview.js
@@ -6,11 +6,14 @@
     HEIGHT: 35
   };
 
+  const COMMENTS_TO_SHOW = 5;
+
   const bigPicture = document.querySelector(`.big-picture`);
   const bigPictureImg = bigPicture.querySelector(`.big-picture__img`).querySelector(`img`);
   const likesCount = bigPicture.querySelector(`.likes-count`);
   const bigPictureDescription = bigPicture.querySelector(`.social__caption`);
-  const commentsCount = bigPicture.querySelector(`.social__comment-count`);
+  const commentsCountShown = bigPicture.querySelector(`.comments-count--shown`);
+  const commentsCountAll = bigPicture.querySelector(`.comments-count--all`);
   const commentsLoader = bigPicture.querySelector(`.comments-loader`);
   const commentsList = bigPicture.querySelector(`.social__comments`);
 
@@ -33,23 +36,46 @@
     return newComment;
   };
 
-  const renderUserComments = (userPost) => {
-    for (let i = 0; i < userPost.comments.length; i++) {
-      commentsList.appendChild(buildComment(userPost.comments[i]));
+  const renderUserComments = (post, from, to) => {
+    for (let i = from; i < to; i++) {
+      commentsList.appendChild(buildComment(post.comments[i]));
     }
+  };
+
+  const shownUserComments = (userPost, commentIndexFrom = 0, commentIndexTo = 5) => {
+    if (userPost.comments.length <= commentIndexTo) {
+      commentIndexTo = userPost.comments.length;
+      commentsLoader.classList.add(`hidden`);
+    } else {
+      commentsLoader.classList.remove(`hidden`);
+
+      const onCommentsLoaderClick = () => {
+        const indexFrom = commentIndexTo;
+        const indexTo = indexFrom + COMMENTS_TO_SHOW;
+
+        commentsLoader.removeEventListener(`click`, onCommentsLoaderClick);
+
+        shownUserComments(userPost, indexFrom, indexTo);
+      };
+
+      commentsLoader.addEventListener(`click`, onCommentsLoaderClick);
+    }
+
+    commentsCountShown.textContent = commentIndexTo;
+
+    renderUserComments(userPost, commentIndexFrom, commentIndexTo);
   };
 
   const renderBigPicture = (userPost) => {
     bigPictureImg.src = userPost.url;
     bigPictureDescription.textContent = userPost.description;
     likesCount.textContent = userPost.likes;
-    commentsCount.textContent = userPost.comments.length;
+    commentsCountAll.textContent = userPost.comments.length;
 
-    renderUserComments(userPost);
+    shownUserComments(userPost);
   };
 
   commentsLoader.classList.add(`hidden`);
-  commentsCount.classList.add(`hidden`);
 
   window.preview = {
     renderBigPicture
